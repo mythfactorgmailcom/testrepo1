84a4f58c26bf0abe276b6219cb5db595f0550bed
mythfactorgmailcom <myth.factor@gmail.com>
2022-09-24 20:47:14 -0400

    v1

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY']="secretkey123"
+
+@app.route("/home")
+def home():
+ return "<h1>Hello world </h1>"
+
+if __name__ == "__main__":
+   app.run()
